db80e2aa6cdbe2ed0df5d13243ebb8a06233f816
Difsan <difsanchezru@gmail.com>
2023-04-22 15:13:54 -0500

    Initial commit

---
+++ src/main/java/com/example/sendEmailSpring/SendEmailSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.sendEmailSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailSpringApplication.class, args);
+	}
+
+}
